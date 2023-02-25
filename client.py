fe453e3c181b36807a53f70867df670112d851e3
snbianco <samantha.n.bianco@vanderbilt.edu>
2023-02-25 17:19:56 -0500

    Add broadcast example, keep track of clients

--- client.py
+++ client.py
@@ -1,5 +1,4 @@
 import socketio
-from flask_socketio import send, emit
 
 sio = socketio.Client()
 
@@ -7,8 +6,17 @@ sio = socketio.Client()
 def handle_connect():
     print('Connected to server')
 
+@sio.on('message')
+def handle_message(data):
+    print(data)
+
+@sio.on('broadcast')
+def handle_broadcast(data):
+    print(data)
+
 sio.connect('http://127.0.0.1:8081')
 
-message = input('What message would you like to send? \n')
-sio.emit('message', message)
+# message = input('What message would you like to send? \n')
+# sio.emit('message', message)
+# sio.emit('event')
 sio.wait()
--- server.py
+++ server.py
@@ -1,17 +1,32 @@
-from flask import Flask
-from flask_socketio import SocketIO
+from flask import Flask, request
+from flask_socketio import SocketIO, emit
 
 app = Flask(__name__)
 socketio = SocketIO(app)
+num_clients = 0
 
 @socketio.on('connect')
 def handle_connect():
-    print('Client connected')
+    global num_clients 
+    num_clients += 1
+    emit('message', f'Welcome to Clue-Less! {num_clients} player(s) connected.')
+    print(f'Client ({request.sid}) connected.')
+
+@socketio.on('disconnect')
+def handle_disconnect():
+    global num_clients 
+    num_clients -= 1
+    print(f'Client ({request.sid}) disconnected.')
 
 @socketio.on('message')
 def handle_message(data):
     print('Received message: ' + data)
 
+@socketio.on('event')
+def handle_event():
+    emit('broadcast', 'This is a broadcast', broadcast=True)
+
 # Port thing might be an issue. We will have to make sure that the server is using a port that isn't already taken.
 if __name__ == '__main__':
+
     socketio.run(app, host='127.0.0.1', port=8081)
